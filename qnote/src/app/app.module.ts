213ac6a509b56cdf400d7adf67f9d7a95707e815
Jimmy <jiaming0708@gmail.com>
2017-12-12 22:25:16 +0800

    add share module

--- qnote/src/app/app.module.ts
+++ qnote/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+import { EntryComponent } from './entry/entry.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
