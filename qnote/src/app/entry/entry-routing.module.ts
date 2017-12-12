86949318bcf5941497c22f477c82ceab0f5fcf1b
Jimmy <jiaming0708@gmail.com>
2017-12-12 23:24:29 +0800

    add entry component & module

---
+++ qnote/src/app/entry/entry-routing.module.ts
@@ -0,0 +1,13 @@
+import { EntryComponent } from './entry.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: EntryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class EntryRoutingModule { }
