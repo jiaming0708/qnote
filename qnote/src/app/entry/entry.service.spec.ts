5a62fe8ae1476aca6302be208c0e7ecfe9d2fe26
Jimmy <jiaming0708@gmail.com>
2017-12-19 23:14:31 +0800

    add test case

--- qnote/src/app/entry/entry.service.spec.ts
+++ qnote/src/app/entry/entry.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { EntryService } from './entry.service';
+import { ServiceBuilder } from 'selenium-webdriver/chrome';
 
 describe('EntryService', () => {
   beforeEach(() => {
@@ -11,5 +12,11 @@ describe('EntryService', () => {
 
   it('should be created', inject([EntryService], (service: EntryService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('input jimmy be the name and get token', inject([EntryService], (service: EntryService) => {
+    service.login('jimmy').subscribe(result => {
+      expect(result).toEqual('jimmy');
+    });
   }));
 });
