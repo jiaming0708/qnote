5754c43e1934a50214221e7267d8d72ad27c4cfa
Jimmy <jiaming0708@gmail.com>
2017-12-20 21:06:34 +0800

    write component test case

--- qnote/src/app/entry/entry-routing.module.ts
+++ qnote/src/app/entry/entry-routing.module.ts
@@ -2,7 +2,7 @@ import { EntryComponent } from './entry.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: EntryComponent }
 ];
 
--- qnote/src/app/entry/entry.component.spec.ts
+++ qnote/src/app/entry/entry.component.spec.ts
@@ -1,21 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { EntryService } from './entry.service';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { EntryComponent } from './entry.component';
+import { FormsModule } from '@angular/forms';
+import { EntryRoutingModule } from './entry-routing.module';
+import { Router } from '@angular/router';
+import { routes } from './entry-routing.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from 'selenium-webdriver';
 
 describe('EntryComponent', () => {
   let component: EntryComponent;
   let fixture: ComponentFixture<EntryComponent>;
+  let entryService: EntryService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EntryComponent ]
+      imports: [
+        FormsModule,
+        EntryRoutingModule,
+        RouterTestingModule.withRoutes(routes)
+      ],
+      declarations: [EntryComponent],
+      providers: [EntryService]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EntryComponent);
+
     component = fixture.componentInstance;
+    entryService = fixture.debugElement.injector.get(EntryService);
+    router = fixture.debugElement.injector.get(Router);
     fixture.detectChanges();
   });
 
@@ -24,9 +42,17 @@ describe('EntryComponent', () => {
   });
 
   it('should have title', () => {
-    const fixture = TestBed.createComponent(EntryComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to QNote!');
+  });
+
+  it('should input name on input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const noteName = compiled.query(By.name('noteName'));
+    // console.log(noteName);
+
+    // component.noteName = 'jimmy';
+    // component.submit();
+
   });
 });
